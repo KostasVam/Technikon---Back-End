dce92be2c3166884558b7854430e79a270caba27
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-02 04:49:28 +0300

    init project

---
+++ Final_Project_ED/src/main/java/com/technikon/final_project_ed/JAXRSConfiguration.java
@@ -0,0 +1,14 @@
+package com.technikon.final_project_ed;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ *
+ * @author Kostas Vamvakousis
+ */
+@ApplicationPath("/")
+public class JAXRSConfiguration extends Application {
+
+}
