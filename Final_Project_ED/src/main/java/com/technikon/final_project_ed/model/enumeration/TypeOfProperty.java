d2f5230e7c9b24016aabc9eca0abae357ab7c11c
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-02 04:45:38 +0300

    created TypeOfProperty class

---
+++ Final_Project_ED/src/main/java/com/technikon/final_project_ed/model/enumeration/TypeOfProperty.java
@@ -0,0 +1,10 @@
+package com.technikon.final_project_ed.model.enumeration;
+
+/**
+ * enum class of Properties
+ *
+ * @author Nikolaos Mpifsas
+ */
+public enum TypeOfProperty {
+    DETACHED_HOUSE, MAISONETTE, APARTMENT_BUILDING
+}
