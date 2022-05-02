5ed0676cee36d1f788a015039af8afeb5b3f72cd
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-02 04:45:49 +0300

    created TypeOfRepair class

---
+++ Final_Project_ED/src/main/java/com/technikon/final_project_ed/model/enumeration/TypeOfRepair.java
@@ -0,0 +1,9 @@
+package com.technikon.final_project_ed.model.enumeration;
+
+/**
+ *
+ * @author marina
+ */
+public enum TypeOfRepair {
+    PAINTING, INSULATION, FRAMES, PLUMBING, ELECTRICAL_WORK
+}
