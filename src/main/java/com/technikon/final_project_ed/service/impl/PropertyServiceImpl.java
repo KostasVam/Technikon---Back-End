1372e3e49ed6a1c6fb1383b2b7cf5f3d56407275
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-05 02:54:57 +0300

    added methods

--- src/main/java/com/technikon/final_project_ed/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/technikon/final_project_ed/service/impl/PropertyServiceImpl.java
@@ -5,17 +5,14 @@ import com.technikon.final_project_ed.dto.RepairDto;
 import com.technikon.final_project_ed.model.Owner;
 import com.technikon.final_project_ed.model.Property;
 import com.technikon.final_project_ed.model.Repair;
-import com.technikon.final_project_ed.model.enumeration.TypeOfProperty;
 import com.technikon.final_project_ed.repository.OwnerRepository;
 import com.technikon.final_project_ed.repository.PropertyRepository;
 import com.technikon.final_project_ed.repository.RepairRepository;
 import com.technikon.final_project_ed.service.PropertyService;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +23,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @NoArgsConstructor
-@Default
-public class PropertyServiceImpl implements PropertyService, Serializable {
+public class PropertyServiceImpl implements PropertyService {
 
     @Inject
     private PropertyRepository propertyRepo;
@@ -36,13 +32,16 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
     @Inject
     private RepairRepository repairRepo;
 
-    public boolean isPropertyValid(Property property) throws IllegalArgumentException {
-        boolean uniqueId = (propertyRepo.findByPropertyId(property.getPropertyID()).isEmpty());
-        if (uniqueId) {
-            return uniqueId;
-        } else {
-            throw new IllegalArgumentException("Not unique property id");
+    private boolean isPropertyValid(long propertyId) {
+        boolean uniqueId = (propertyRepo.findByPropertyId(propertyId).isEmpty());
+        if (!uniqueId) {
+            log.info("Not unique property id");
         }
+        return uniqueId;
+    }
+
+    private PropertyDto getDummyPropertyDto() {
+        return new PropertyDto(new Property.Builder().setId(-1L).build());
     }
 
     /**
@@ -50,26 +49,31 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
      * calls the save method from the propertyRepository to add the given
      * property to the database.
      *
-     * @param property
+     * @param propertyDto
      * @return
      */
     @Override
-    public PropertyDto create(Property property) {
+    public PropertyDto create(PropertyDto propertyDto) {
         try {
-            if (isPropertyValid(property) && ownerRepo.findByVat(property.getOwnerVat()) != null) {
-                propertyRepo.save(property);
-                return new PropertyDto(property);
+            if (ownerRepo == null) {
+                log.info("null owner repo");
+            }
+            if (propertyRepo == null) {
+                log.info("null propertyRepo");
+            }
+            if (propertyDto == null) {
+                log.info("null propertyDto");
+            }
+            if (isPropertyValid(propertyDto.getPropertyID())) { //&& ownerRepo.findByVat(propertyDto.getOwnerVat()) != null
+                return new PropertyDto(propertyRepo.save(propertyDto.createProperty()).get());
             } else {
-                log.info("Property with id: {} is invalid", property.getId());
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
+                log.info("Property with id: {} is invalid", propertyDto.getPropertyID());
+                return getDummyPropertyDto();
             }
         } catch (NullPointerException e) {
             log.info("Invalid null property");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
+            e.printStackTrace();
+            return getDummyPropertyDto();
         }
     }
 
@@ -77,16 +81,16 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
      * create: iterates the given list of property and for each owner it calls
      * the create method with the iterated property as a parameter.
      *
-     * @param propertyList
+     * @param propertyDtoList
      * @return
      */
     @Override
-    public List<PropertyDto> create(List<Property> propertyList) {
-        List<PropertyDto> propertyDtoList = new ArrayList<>();
-        for (Property property : propertyList) {
-            propertyDtoList.add(create(property));
+    public List<PropertyDto> create(List<PropertyDto> propertyDtoList) {
+        List<PropertyDto> propertyCreatedList = new ArrayList<>();
+        for (PropertyDto propertyDto : propertyDtoList) {
+            propertyCreatedList.add(create(propertyDto));
         }
-        return propertyDtoList;
+        return propertyCreatedList;
     }
 
     /**
@@ -103,9 +107,7 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
             return new PropertyDto(propertyFound.get());
         }
         log.info("Property with id {} wasn't found", id);
-        Property propertyReturned = new Property();
-        propertyReturned.setId(-1L);
-        return new PropertyDto(propertyReturned);
+        return getDummyPropertyDto();
     }
 
     /**
@@ -122,9 +124,7 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
             return new PropertyDto(propertyFound.get());
         }
         log.info("Property with property id {} wasn't found", propertyId);
-        Property propertyReturned = new Property();
-        propertyReturned.setId(-1L);
-        return new PropertyDto(propertyReturned);
+        return getDummyPropertyDto();
     }
 
     /**
@@ -154,22 +154,6 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
     }
 
     /**
-     * printAllOwners: calls the getAll method to retrieve all the properties
-     * stored in database.
-     */
-    @Override
-    public void printAllProperties() {
-        List<Property> propertyList = propertyRepo.findAll();
-        if (!propertyList.isEmpty()) {
-            for (Property property : propertyList) {
-                System.out.println(property);
-            }
-        } else {
-            log.info("There are no properties");
-        }
-    }
-
-    /**
      * getAll: calls the ownerRepo's method readAll to retrieve all the
      * properties in the database
      *
@@ -188,162 +172,47 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
         return propertyDtoList;
     }
 
-    /**
-     * updateId: checks if the given property corresponds to an existing
-     * property in the database.If that property is found then the updateId sets
-     * newId with the given Long
-     *
-     *
-     * @param id
-     * @param newΙd
-     * @return
-     */
     @Override
-    public PropertyDto updatePropertyId(long id, Long newΙd) {
+    public PropertyDto update(PropertyDto propertyDto) {
         try {
-            Optional<Property> propertyFound = propertyRepo.findByPropertyId(id);
-            Optional<Property> newIdFound = propertyRepo.findByPropertyId(newΙd);
-            if (propertyFound.isPresent() && newIdFound.isEmpty() && newΙd != null) {
-                propertyFound.get().setPropertyID(newΙd);
-                propertyRepo.update(id, propertyFound.get());
-                return new PropertyDto(propertyFound.get());
+            Optional<Property> propertyFound = propertyRepo.findById(propertyDto.getId());
+            if (propertyFound.isPresent()) {
+                log.info("Property was found");
+                if (propertyDto.getPropertyID() != null && isPropertyValid(propertyDto.getPropertyID())) {
+                    propertyFound.get().setPropertyID(propertyDto.getPropertyID());
+                    log.info("PropertyId was updated");
+                }
+                if (propertyDto.getAddress() != null) {
+                    propertyFound.get().setAddress(propertyDto.getAddress());
+                    log.info("Address was updated");
+                }
+                if (propertyDto.getYearOfConstruction() != null
+                        && propertyDto.getYearOfConstruction() <= Calendar.getInstance().get(Calendar.YEAR)) {
+                    propertyFound.get().setYearOfConstruction(propertyDto.getYearOfConstruction());
+                    log.info("Year Of Construction was updated");
+                }
+                if (propertyDto.getTypeOfProperty() != null) {
+                    propertyFound.get().setTypeOfProperty(propertyDto.getTypeOfProperty());
+                    log.info("Type Of Property was updated");
+                }
+//                if (propertyDto.getOwnerVat() != null) {
+//                    Optional<Owner> ownerFound = ownerRepo.findByVat(propertyDto.getOwnerVat());
+//                    if (ownerFound.isPresent()) {
+//                        propertyFound.get().setOwnerVat(propertyDto.getOwnerVat());
+//                        propertyFound.get().setOwner(ownerFound.get());
+//                    } else {
+//                        log.info("Owner's vat wasn't found");
+//                    }
+//                    log.info("Owner was updated");
+//                }
+                return new PropertyDto(propertyRepo.update(propertyFound.get().getId(), propertyFound.get()).get());
             } else {
-                log.info("Invalid id or property.");
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
+                log.info("Property wasn't found");
+                return getDummyPropertyDto();
             }
         } catch (NullPointerException ex) {
             log.info("Invalid null property.");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
-        }
-    }
-
-    /**
-     * updateOwner: checks if the given owner corresponds to an existing owner
-     * in the database.If that owner is found then the updateOwner sets newOwner
-     * with the given Owner
-     *
-     * @param propertyId
-     * @param ownerId
-     * @return
-     */
-    @Override
-    public PropertyDto updateOwner(long propertyId, long ownerId) {
-        try {
-            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
-            Optional<Owner> ownerFound = ownerRepo.findById(ownerId);
-            if (ownerFound.isPresent() && propertyFound.isPresent()) {
-                propertyFound.get().setOwner(ownerFound.get());
-                propertyRepo.update(propertyId, propertyFound.get());
-                return new PropertyDto(propertyFound.get());
-            } else {
-                log.info("Invalid property or owner.");
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
-            }
-        } catch (NullPointerException e) {
-            log.info("Invalid null property or owner.");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
-        }
-
-    }
-
-    /**
-     * updateAddress: checks if the given property corresponds to an existing
-     * property in the database.If that property is found then the updateAddress
-     * sets newAddress with the given String
-     *
-     * @param propertyId
-     * @param newAddress
-     * @return
-     */
-    @Override
-    public PropertyDto updateAddress(long propertyId, String newAddress) {
-        try {
-            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
-            if (propertyFound.isPresent() && newAddress != null) {
-                propertyFound.get().setAddress(newAddress);
-                propertyRepo.update(propertyId, propertyFound.get());
-                return new PropertyDto(propertyFound.get());
-            } else {
-                log.info("Invalid property or address.");
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
-            }
-        } catch (NullPointerException e) {
-            log.info("Invalid null property.");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
-        }
-    }
-
-    /**
-     * updateYearOfConstruction: checks if the given property corresponds to an
-     * existing property in the database.If that property is found then the
-     * updateYearOfConstruction sets newYearOfConstruction with the given String
-     *
-     * @param propertyId
-     * @param newYearOfConstruction
-     * @return
-     */
-    @Override
-    public PropertyDto updateYearOfConstruction(long propertyId, Integer newYearOfConstruction) {
-        try {
-            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
-            if (propertyFound.isPresent() && newYearOfConstruction <= Calendar.getInstance().get(Calendar.YEAR)) {
-                propertyFound.get().setYearOfConstruction(newYearOfConstruction);
-                propertyRepo.update(propertyId, propertyFound.get());
-                return new PropertyDto(propertyFound.get());
-            } else {
-                log.info("Invalid property or year of construction.");
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
-            }
-        } catch (NullPointerException e) {
-            log.info("Invalid null property.");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
-        }
-    }
-
-    /**
-     * updateTypeOfProperty: checks if the given property corresponds to an
-     * existing property in the database.If that property is found then the
-     * updateTypeOfProperty sets newType with the given TypeOfProperty
-     *
-     * @param propertyId
-     * @param newType
-     * @return
-     */
-    @Override
-    public PropertyDto updateTypeOfProperty(long propertyId, TypeOfProperty newType) {
-        try {
-            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
-            if (propertyFound.isPresent() && newType != null) {
-                propertyFound.get().setTypeOfProperty(newType);
-                propertyRepo.update(propertyId, propertyFound.get());
-                return new PropertyDto(propertyFound.get());
-            } else {
-                log.info("Invalid property or type of property.");
-                Property propertyReturned = new Property();
-                propertyReturned.setId(-1L);
-                return new PropertyDto(propertyReturned);
-            }
-        } catch (NullPointerException e) {
-            log.info("Invalid null property.");
-            Property propertyReturned = new Property();
-            propertyReturned.setId(-1L);
-            return new PropertyDto(propertyReturned);
+            return getDummyPropertyDto();
         }
     }
 
@@ -367,25 +236,6 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
 
     }
 
-    @Override
-    public List<RepairDto> findRepairsOfProperty(long propertyId) {
-        List<RepairDto> repairDtosList = new ArrayList<>();
-        try {
-            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
-            if (propertyFound.isPresent()) {
-                List<Repair> repairList = propertyRepo.findRepairByProperty(propertyFound.get());
-                for (Repair repair : repairList) {
-                    repairDtosList.add(new RepairDto(repair));
-                }
-            } else {
-                log.info("Invalid property.");
-            }
-        } catch (NullPointerException e) {
-            log.info("Invalid null property.");
-        }
-        return repairDtosList;
-    }
-
     /**
      * delete: calls the getAll method to retrieve all the properties registered
      * in the database. And delete all the properties with all repairs
@@ -407,6 +257,25 @@ public class PropertyServiceImpl implements PropertyService, Serializable {
         } catch (NullPointerException e) {
             log.info("Database hasn't any properties.");
         }
+    }
+
+    @Override
+    public List<RepairDto> findRepairsOfProperty(long propertyId) {
+        List<RepairDto> repairDtosList = new ArrayList<>();
+        try {
+            Optional<Property> propertyFound = propertyRepo.findById(propertyId);
+            if (propertyFound.isPresent()) {
+                List<Repair> repairList = propertyRepo.findRepairByProperty(propertyFound.get());
+                for (Repair repair : repairList) {
+                    repairDtosList.add(new RepairDto(repair));
+                }
+            } else {
+                log.info("Invalid property.");
+            }
+        } catch (NullPointerException e) {
+            log.info("Invalid null property.");
+        }
+        return repairDtosList;
     }
 
 }
