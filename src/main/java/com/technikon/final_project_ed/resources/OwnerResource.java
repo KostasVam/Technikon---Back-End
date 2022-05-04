e46b986f15c7cfd9891a73e549c9ac5e02544271
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-05 02:53:58 +0300

    fixed endpoints

--- src/main/java/com/technikon/final_project_ed/resources/OwnerResource.java
+++ src/main/java/com/technikon/final_project_ed/resources/OwnerResource.java
@@ -2,8 +2,8 @@ package com.technikon.final_project_ed.resources;
 
 import com.technikon.final_project_ed.dto.OwnerDto;
 import com.technikon.final_project_ed.service.OwnerService;
-import com.technikon.final_project_ed.usecases.OwnerUseCase;
-import java.util.List;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -20,58 +20,48 @@ public class OwnerResource {
     @Inject
     private OwnerService ownerService;
 
-//    @Inject
-//    public OwnerResource(OwnerService ownerService) {
-//        this.ownerService = ownerService;
-//    }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<OwnerDto> getAll() {
-        return ownerService.getAll();
+    public Response getAll() {
+        return Response.ok().entity(ownerService.getAll()).build();
     }
 
-    @Path("owner")
+    @Path("/{vat}")
+    @PermitAll
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOwnerByVat(@QueryParam("vat") long ownerVat) {
+    public Response getOwnerByVat(@PathParam("vat") long ownerVat) {
         return Response.ok().entity(ownerService.searchByVat(ownerVat)).build();
     }
 
-    @Path("useCaseCreate")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response useCaseCreate(@QueryParam("vat") long ownerVat) {
-        OwnerUseCase uc = new OwnerUseCase();
-        uc.createOwnersUseCases();
-        return Response.ok().build();
-    }
-
-//    @Path("/")
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public OwnerDto getOwnerByEmail(@QueryParam("email") String email) {
-//        return ownerService.searchByEmail(email);
-//    }
-//    @Path("/{ownerVat}")
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public OwnerDto getOwner2(@PathParam("ownerVat") long ownerVat) {
-//        return ownerService.searchByVat(ownerVat);
-//    }
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public OwnerDto saveEmployee(@FormParam("vat") long ownerVat, @FormParam("email") String email) {
-        log.info("vat: {}, email: {}", ownerVat, email);
-        return ownerService.create(ownerVat, email);
+    @RolesAllowed("ADMIN")
+    public Response saveEmployee(OwnerDto ownerDto) {
+        log.info("vat: {}, email: {}", ownerDto.getVat(), ownerDto.getEmail());
+        return Response.ok().entity(ownerService.create(ownerDto)).build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
+    public Response updateProduct(OwnerDto ownerDto) {
+        return Response.ok().entity(ownerService.update(ownerDto)).build();
     }
 
     @Path("/{id}")
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
     public void deleteOwner(@PathParam("id") long id) {
         ownerService.delete(id);
     }
 
+    @DELETE
+    @RolesAllowed("ADMIN")
+    public void deleteAll() {
+        ownerService.deleteAll();
+    }
 }
