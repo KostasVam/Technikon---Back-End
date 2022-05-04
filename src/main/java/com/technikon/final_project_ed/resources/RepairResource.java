79246ce50d89e6449aa65b3a4aabf19104cf452b
Kostas Vamvakousis <vamvakousis9292@gmail.com>
2022-05-05 02:54:08 +0300

    fixed endpoints

---
+++ src/main/java/com/technikon/final_project_ed/resources/RepairResource.java
@@ -0,0 +1,72 @@
+package com.technikon.final_project_ed.resources;
+
+import com.technikon.final_project_ed.dto.RepairDto;
+import com.technikon.final_project_ed.service.RepairService;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Path("repairs")
+@Slf4j
+@NoArgsConstructor
+public class RepairResource {
+
+    @Inject
+    private RepairService repairService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        return Response.ok().entity(repairService.getAll()).build();
+    }
+
+    @Path("/{id}")
+    @PermitAll
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRepair(@PathParam("id") long id) {
+        return Response.ok().entity(repairService.searchById(id)).build();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
+    public Response saveRepair(RepairDto repairDto) {
+        if (repairService == null) {
+            log.info("null service");
+        }
+        if (repairDto == null) {
+            log.info("null propertyDto");
+        }
+        return Response.ok().entity(repairService.create(repairDto)).build();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
+    public Response updateRepair(RepairDto repairDto) {
+        return Response.ok().entity(repairService.update(repairDto)).build();
+    }
+
+    @Path("/{id}")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @RolesAllowed("ADMIN")
+    public void deleteProperty(@PathParam("id") long id) {
+        repairService.delete(id);
+    }
+
+    @DELETE
+    @RolesAllowed("ADMIN")
+    public void deleteAll() {
+        repairService.deleteAll();
+    }
+}
